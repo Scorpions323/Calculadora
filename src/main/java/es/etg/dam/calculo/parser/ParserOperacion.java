22e05c57c15c21937d114a219002074c79120465
Scorpions323 <rolonaus@gmail.com>
2026-05-29 10:58:42 +0200

    Arreglo de Exception

--- src/main/java/es/etg/dam/calculo/parser/ParserOperacion.java
+++ src/main/java/es/etg/dam/calculo/parser/ParserOperacion.java
@@ -1,8 +1,8 @@
 package es.etg.dam.calculo.parser;
 
-import es.etg.dam.exception.ParserException;
+import es.etg.dam.exception.ParserOperacionException;
 import es.etg.dam.calculo.Operacion;
 
 public interface ParserOperacion {
-    Operacion parsear(String mensaje) throws ParserException;
+    Operacion parsear(String mensaje) throws ParserOperacionException;
 }
