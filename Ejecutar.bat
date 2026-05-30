0b51336e5218a5c41374d280b4ca877b7213bb6f
Scorpions323 <rolonaus@gmail.com>
2026-05-30 12:16:53 +0200

    Mejora Cifrado

--- Ejecutar.bat
+++ Ejecutar.bat
@@ -5,6 +5,6 @@ start cmd /k java -cp target/classes es.etg.dam.server.Servidor
 timeout /t 2 >nul
 
 java -cp target/classes es.etg.dam.client.Cliente 10 + 5
-java -cp target/classes es.etg.dam.client.Cliente 20 - 4
+java -cp target/classes es.etg.dam.client.Cliente 20 - 5
 
 pause
